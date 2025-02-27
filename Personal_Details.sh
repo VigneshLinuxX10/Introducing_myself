3a20c8b75c05fa815c478502624fec6851f72c01
VigneshLinuxX10 <vignesh.github842@gmail.com>
2025-02-27 13:28:51 +0000

    INTRODUCING_MYSELF

---
+++ Personal_Details.sh
@@ -0,0 +1,22 @@
+
+print("===================================")
+
+print("INTRODUCING MYSELF")
+
+print("===================================")
+
+print("\nHello This is Vignesh new to Github")
+
+
+def personal_detail(name,profession,user_name):
+         print(f"\nHi! there myself {name}")
+         print(f"\nI work  as a {profession}")
+         print(f"\nusername : {user_name}")
+
+name = "Vignesh"
+profession = "Linux Engineer"
+user_name = "VigneshLinuxX10"
+PD = personal_detail(name,profession,user_name)
+print(PD)
+
+print("\nYou can see my github page :github.com/VigneshLinuxX10")
